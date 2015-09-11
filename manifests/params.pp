c03417a9c66e6ed84878329ae495b39b25b2b44b
Geoff Williams <geoff.williams@puppetlabs.com>
2015-09-11 12:52:49 +1000

    dummy client and params added

---
+++ manifests/params.pp
@@ -0,0 +1,6 @@
+class puppet_enterprise::params {
+  $mco_create_client_user       = "peadmin"
+  $stomp_password               = "stomp_password_value"
+  $mcollective_middleware_port  = 61616
+  $stomp_user                   = "stomp_user_value"
+}
